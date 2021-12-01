e87121a4ecc1b6f8d9ae5e9485449407d0fac2fb
MohammedQuraysh <mohammedquraysh4@gmail.com>
2021-12-01 12:18:43 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const[name, setName] = useState('Mohammed Quraysh')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>my name is {name}</Text>
+      <Button title='Update' onPress={()=>{setName('Musa')}}></Button>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width:'100%',
+    height: '50%',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderColor: '#fff00f',
+    borderWidth: 10,
+    borderRadius: 20,
+
+  
   },
+  text:{
+    color: '#ff0fff',
+    fontSize: 20,
+    fontWeight: '700', 
+    fontStyle: "italic",
+    margin:10,
+    textTransform:'uppercase',
+    textShadowColor: '#fff00f'
+
+  }
 });
